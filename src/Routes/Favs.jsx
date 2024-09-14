67dfe3af8ca914ef375207df6c3f3e3e974c2e05
HansUrpay <hansurpayh@gmail.com>
2024-09-14 01:30:30 -0500

    styles: add styles in Favs

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -9,11 +9,11 @@ const Favs = () => {
   console.log("PRUEBA: ");
 
   return (
-    <div className="flex-1 dark:bg-bg_secondary">
-      <h1 className="text-center text-3xl font-bold text-bg_secondary dark:text-primary mt-16 mb-8">
+    <div className="flex-1 dark:bg-bg_secondary py-8">
+      <h1 className="text-center text-3xl font-bold text-bg_secondary dark:text-primary mt-10 mb-8">
         Favoritos
       </h1>
-      <div className="card-grid">
+      <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 place-items-center gap-x-4 gap-y-8 w-11/12 mx-auto">
         {/* este componente debe consumir los destacados del localStorage /}
         {/ Deberan renderizar una Card por cada uno de ellos */}
         {state.favorites.map((item) => (
